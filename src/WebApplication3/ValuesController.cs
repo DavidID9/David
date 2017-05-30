9109ceda3af37b67b5a23fa787c012b019f71ee9
David <irimciuc_david@yahoo.ro>
2017-05-30 10:04:25 +0300

    First commit

---
+++ src/WebApplication3/ValuesController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Azure; // Namespace for CloudConfigurationManager
+using Microsoft.WindowsAzure.Storage; // Namespace for CloudStorageAccount
+using Microsoft.WindowsAzure.Storage.Blob; // Namespace for Blob storage types
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication3
+{
+    public class ValuesController
+    {
+        [Route("api/[controller]")]
+
+        private readonly CloudBlobClient blobClient;
+        public ValuesController(IConfigurationRoot config)
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+    CloudConfigurationManager.GetSetting("StorageConnectionString"));
+
+
+            // Create the blob client.
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+        }
+        // GET api/values
+        [HttpGet]
+        public async static Task ListBlobsSegmentedInFlatListing(CloudBlobContainer container)
+        {
+            // Retrieve reference to a previously created container.
+            CloudBlobContainer container = blobClient.GetContainerReference("mycontainer");
+
+        }
+
+
+
+    }
+}
